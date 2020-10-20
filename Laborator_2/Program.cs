f13942e6b44cc7ee2114d61b9ad36e968667f018
Trip Daria <daria.trip001@gmail.com>
2020-10-20 21:21:45 +0300

    lab2

---
+++ Laborator_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vezi poze folder de la curs 
+            //TODO transcrie problemele din poze 
+            //TODO tema
+            
+        }
+    }
+}
