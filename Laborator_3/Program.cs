4ff573d947768d3e1d1436b5b4d808ae7a3f31bc
Trip Daria <daria.trip001@gmail.com>
2020-11-01 00:04:43 +0200

    sum

--- Laborator_3/Program.cs
+++ Laborator_3/Program.cs
@@ -294,7 +294,7 @@ namespace Reuniune
 
         static void Tema()
         {
-            int a, b, nr1, nr2;
+            /*int a, b, nr1, nr2;
             a = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
             nr1 = a;
@@ -424,7 +424,57 @@ namespace Reuniune
             }
 
             for (int i = 0; i < max; i++)
-                Console.Write(Suma[i] + " "); 
+                Console.Write(Suma[i] + " "); */
+
+
+
+            //Solutia lui Tepes Alexandru
+
+            // cei doi vectori initiali
+            List<int> v1 = new List<int> { 1, 6, 1, 0 };
+            List<int> v2 = new List<int> { 1, 4, 4 };
+
+            // vectorul rezultat
+            List<int> v3 = new List<int>();
+
+            // inversam ordinea celor doi vectori
+            v1.Reverse();
+            v2.Reverse();
+
+            // initial, vectorul final va fi egal cu vectorul initial cel mai lung
+            if (v1.Count > v2.Count) v3 = v1;
+            else v3 = v2;
+
+            // adunuam cei doi vectori si retinem restul
+            int rest = 0, lmin = Math.Min(v1.Count, v2.Count), i;
+            for (i = 0; i < lmin; i++)
+            {
+                int x = v1[i]; int y = v2[i];
+                v3[i] = (x + y + rest) % 10;
+                rest = (x + y) / 10;
+            }
+
+            // daca la final a mai ramas rest, il punem in rezultat
+            if (rest > 0) v3.Add(0);
+            while (rest > 0)
+            {
+                v3[i] = v3[i] + rest;
+                int x = v3[i];
+                rest = x / 10;
+                v3[i] %= 10;
+                i++;
+            }
+
+            // inversam rezultatul 
+            v3.Reverse();
+
+            // afisam rezultatul
+            for (i = 0; i < v3.Count; i++)
+            {
+                Console.Write(v3[i]);
+            }
+
+            Console.ReadKey();
 
 
 
