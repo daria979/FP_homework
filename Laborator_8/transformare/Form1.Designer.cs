d9703cdc4d7072eeebabbf748e6ab22319611208
daria trip <daria.trip@outlook.com>
2020-12-05 22:26:53 +0200

    Din cifre romane in arabe (Formular)

---
+++ Laborator_8/transformare/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace transformare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 63);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 63);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(88, 63);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(80, 119);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(126, 144);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(104, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Month";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 269);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(235, 134);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(143, 228);
+            this.btn_calculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(100, 37);
+            this.btn_calculate.TabIndex = 7;
+            this.btn_calculate.Text = "CALCULATE";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(11, 228);
+            this.btn_clear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(100, 37);
+            this.btn_clear.TabIndex = 8;
+            this.btn_clear.Text = "CLEAR";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Day";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 46);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Year";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 85);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 20);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Enter numbers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(254, 413);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Roman Numeral Date Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Laborator_8/transformare/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace transformare
+{
+    public partial class Form1 : Form
+    {
+
+        string convert(int i, string A, string B, string C)
+        {
+            string tor = "";
+            switch (i)
+            {
+                case 1: tor += A; break;
+                case 2: tor += A + A; break;
+                case 3: tor += A + A + A; break;
+                case 4: tor += A + B; break;
+                case 5: tor += B; break;
+                case 6: tor += B + A; break;
+                case 7: tor += B + A + A; break;
+                case 8: tor += B + A + A + A; break;
+                case 9: tor += A + C; break;
+            }
+            return tor;
+        }
+
+        string convert_numeral(int a)
+        {
+            int[] v = new int[5];
+            int i = 0;
+            while (a != 0)
+            {
+                v[i] = a % 10;
+                a /= 10;
+                i++;
+            }
+
+            string tor = "";
+            tor += convert(v[4], "[X]", "NULL", "NULL");
+            tor += convert(v[3], "M", "[V]", "[X]");
+            tor += convert(v[2], "C", "D", "M");
+            tor += convert(v[1], "X", "L", "C");
+            tor += convert(v[0], "I", "V", "X");
+            return tor;
+        }
+
+        int dd = -1;
+        int mm = -1;
+        int yy = -1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+            try { mm = (int)numericUpDown1.Value; }
+                catch{}
+            try { dd = int.Parse(textBox3.Text); }
+                catch { }
+            try { yy = int.Parse(textBox2.Text); }
+                catch { }
+
+            if (dd == -1 && mm == -1 && yy == -1)
+            {
+                listBox1.Items.Add("Answer");
+                listBox1.Items.Add("Enter at least one value for month, day or year.");
+            }
+            else 
+            {
+
+                
+                bool ddok = false;
+                bool mmok = false;
+                bool yyok = false;
+
+                if (dd >= 1 && dd <= 31)
+                    ddok = true;
+                if (mm >= 1 && mm <= 12)
+                    mmok = true;
+                if (yy >= 1 && yy <= 39999)
+                    yyok = true;
+
+                if (ddok && mmok && yyok)
+                {
+                    string output = "";
+                    string separator = "";
+                    string dd_ = convert_numeral(dd);
+                    string mm_ = convert_numeral(mm);
+                    string yy_ = convert_numeral(yy);
+
+
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 0:
+                            separator = ".";
+                            break;
+                        case 1:
+                            separator = "/";
+                            break;
+
+                    }
+
+                    switch (comboBox1.SelectedIndex)
+                    {
+                        case 0:
+                            output = mm_ + separator + dd_ + separator + yy_;
+                            break;
+                        case 1:
+                            output = dd_ + separator + mm_ + separator + yy_;
+                            break;
+                        case 2:
+                            output = yy_ + separator + mm_ + separator + dd_;
+                            break;
+                    }
+
+                    listBox1.Items.Add("Answer : ");
+                    listBox1.Items.Add("\t" + output);
+
+                    
+                }
+                else 
+                {
+                    if (!mmok)
+                        listBox1.Items.Add("Enter a valid month from 1 to 12");
+                    if (!ddok)
+                        listBox1.Items.Add("Enter a valid day from 1 to 31");
+                    if (!yyok)
+                        listBox1.Items.Add("Enter a valid year from 1 to 3999");
+                }
+
+
+            }
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Font = new System.Drawing.Font("Courier New", 12, FontStyle.Regular);
+
+            comboBox1.Items.Add("MM.DD.YYYY (US)");
+            comboBox1.Items.Add("DD.MM.YYYY (Europe)");
+            comboBox1.Items.Add("YYYY.MM.DD (ISO)");
+            comboBox1.SelectedIndex = 0;
+
+            comboBox2.Items.Add(". (dot)");
+            comboBox2.Items.Add("/ (slash)");
+            comboBox2.SelectedIndex = 0;
+
+            numericUpDown1.Maximum = 12;
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Value = 1;
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            listBox1.Items.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
