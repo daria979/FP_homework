776ef98eb3e20703b91b5e2471185842a71d1b43
daria trip <daria.trip@outlook.com>
2021-01-08 13:49:10 +0200

    lab_11 avansati: interclasare si frecventa

---
+++ Interclasare/Laborator_11/Frecventa.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Laborator_11
+{
+    class Frecventa
+    {
+        public struct Data
+        {
+            public int count;
+            public int pos;
+        };
+
+        static int n;
+        static int[] v1;
+        static int maxValue = int.MinValue;
+        static void Load(string fileName)
+        {
+            TextReader load = new StreamReader(fileName);
+            string[] buffer = load.ReadLine().Split(' ');
+            n = int.Parse(buffer[0]);
+            v1 = new int[n];
+            buffer = (load.ReadLine()).Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                v1[i] = int.Parse(buffer[i]);
+                if (v1[i] > maxValue)
+                    maxValue = v1[i];
+            }
+        }
+
+        static void View(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+                Console.Write($"{a[i]} ");
+            Console.WriteLine();
+        }
+
+        static void frecventa(int[] a)
+        {
+            Data[] freq = new Data[maxValue + 1];
+
+            for (int i = 0; i < freq.Length; i++)
+                freq[i].pos = i;
+
+            for (int i = 0; i < n; i++)
+                freq[a[i]].count++;
+
+            //MergeSort(freq, freq[0].count, freq[maxValue].count);
+            Data aux;
+            bool ok;
+            do
+            {
+                ok = true;
+                for (int i = 0; i < freq.Length - 1; i++)
+                    if (freq[i].count < freq[i + 1].count)
+                    {
+                        aux = freq[i];
+                        freq[i] = freq[i + 1];
+                        freq[i + 1] = aux;
+                        ok = false;
+                    }
+            }
+            while (!ok);
+
+            for (int i = 0; i < freq.Length; i++)
+            {
+                if (freq[i].count != 0)
+                    Console.Write($"{freq[i].pos} ");
+            }
+        }
+
+        static void MergeSort(Data[] a, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+                MergeSort(a, left, mid);
+                MergeSort(a, mid + 1, right);
+                Interclasare(a, left, mid, right);
+            }
+        }
+        static void Interclasare(Data[] a, int left, int mid, int right)
+        {
+            Data[] b = new Data[right - left + 1];
+            int i = left, j = mid + 1, k = 0;
+            while (i <= mid && j <= right)
+            {
+                if (a[i].count <= a[j].count)
+                {
+                    b[k] = a[i];
+                    k++;
+                    i++;
+                }
+                else
+                {
+                    b[k] = a[j];
+                    k++;
+                    j++;
+                }
+            }
+            while (i <= mid)
+            {
+                b[k] = a[i];
+                k++;
+                i++;
+            }
+            while (j <= right)
+            {
+                b[k] = a[j];
+                k++;
+                j++;
+            }
+            for (i = 0; i < k; i++)
+                a[left + i] = b[i];
+        }
+        static void Main(string[] args)
+        {
+            Load("../../data2.txt");
+            View(v1);
+            frecventa(v1);
+        }
+    }
+}
---
+++ Interclasare/Laborator_11/Interclasare.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Laborator_11
+{
+    class Interclasare
+    {
+        static int n1, n2;
+        static int[] v1;
+        static int[] v2;
+        static int[] v3;
+
+        static void load(string fileName)
+        {
+            TextReader Load = new StreamReader(fileName);
+            string[] buffer;
+            buffer = (Load.ReadLine()).Split(' ');
+            n1 = int.Parse(buffer[0]);
+            n2 = int.Parse(buffer[1]);
+            v1 = new int[n1];
+            v2 = new int[n2];
+
+            buffer = (Load.ReadLine()).Split(' ');
+            for (int i = 0; i < n1; i++)
+                v1[i] = int.Parse(buffer[i]);
+
+            buffer = (Load.ReadLine()).Split(' ');
+            for (int i = n2 - 1; i >= 0; i--)
+                v2[i] = int.Parse(buffer[i]);
+        }
+
+        static void solve_interclasare()
+        {
+            int[] tmp = new int[n1 + n2];
+            int k1 = 0;
+            int k2 = n2 - 1;
+            int k3 = 0;
+
+            while (k1 < n1 && k2 >= 0)
+            {
+                if (v1[k1] < v2[k2])
+                {
+                    if ((v1[k1] % 2) == 0)
+                    {
+                        tmp[k3] = v1[k1];
+                        k3++;
+                    }
+                    k1++;
+                }
+                else
+                {
+                    if ((v2[k2] % 2) == 0)
+                    {
+                        tmp[k3] = v2[k2];
+                        k3++;
+                    }
+                    k2--;
+                }
+            }
+            while (k1 < n1)
+            {
+                if ((v1[k1]) % 2 == 0)
+                {
+                    tmp[k3] = v1[k1];
+                    k3++;
+                }
+                k1++;
+            }
+            while (k2 >= 0)
+            {
+                if ((v2[k2]) % 2 == 0)
+                {
+                    tmp[k3] = v2[k2];
+                    k3++;
+                }
+                k2--;
+
+            }
+
+            v3 = new int[k3];
+            for (int i = 0; i < k3; i++)
+                v3[i] = tmp[i];
+        }
+
+        static void view(int[] tov)
+        {
+            for (int i = 0; i < tov.Length; i++)
+                Console.Write(tov[i] + " ");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            load(@"..\..\data.txt");
+            view(v1);
+            view(v2);
+            solve_interclasare();
+            view(v3);
+            Console.ReadKey();
+        }
+    }
+}
